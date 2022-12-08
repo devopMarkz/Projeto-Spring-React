0cf8b7e5630e5a5eeed78985c3050f3bdb1b2984
Marcos André <marcosdev2002@gmail.com>
2022-12-08 20:17:04 -0300

    Removendo @

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,10 +7,6 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
-                <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-                </p>
             </div>
         </header>
     )
